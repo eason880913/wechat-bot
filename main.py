62213eb8b5e01fa36001fac2180238ee5db8e339
Eason Lee <eason88091372104@gmail.com>
2019-08-07 18:00:11 +0800

    ok

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def wechat():
             # 新浪SAE未实名用户加上上面这句
             return response
 
-#import os
+import os
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port='80')
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
