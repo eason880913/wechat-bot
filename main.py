adfd95d5041426c89844c190592cb53edbd71bc4
Eason Lee <eason88091372104@gmail.com>
2019-08-07 16:59:59 +0800

    ok

---
+++ main.py
@@ -0,0 +1,39 @@
+import hashlib
+
+from flask import Flask, request, make_response
+import xml.etree.ElementTree as ET
+
+WX_TOKEN = 'fancy'
+# 这里填写公众号配置的token
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/wechat_api/', methods=['GET', 'POST'])
+# 定义路由地址请与URL后的保持一致
+def wechat():
+    if request.method == 'GET':
+    token = WX_TOKEN
+    data = request.args
+    signature = data.get('signature', '')
+    timestamp = data.get('timestamp', '')
+    nonce = data.get('nonce', '')
+    echostr = data.get('echostr', '')
+    s = sorted([timestamp, nonce, token])
+    # 字典排序
+    s = ''.join(s)
+    if hashlib.sha1(s.encode('utf-8')).hexdigest() == signature:
+        # 判断请求来源，并对接受的请求转换为utf-8后进行sha1加密
+        response = make_response(echostr)
+        # response.headers['content-type'] = 'text' 
+        # 新浪SAE未实名用户加上上面这句
+        return response
+
+if __name__ == '__main__':
+app.run()
+# app.run(host='0.0.0.0', port=5050)
